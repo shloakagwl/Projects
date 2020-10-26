4a8ce5240fb5850ffcd7663bbfeb4211f592c37d
shloakagwl <shloakagwl@users.noreply.github.com>
2020-10-26 18:52:24 -0500

    Create main.py

---
+++ Py/Maximize_Distance_to_Closest_Person/main.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        ans = seats.index(1)
+        seats.reverse()
+        ans = max(ans,seats.index(1))
+        for seat, group in itertools.groupby(seats):
+            if not seat:
+                K = len(list(group))
+                ans = max(ans, (K+1)/2)
+
+        return ans
+        
+print(s.maxDistToClosest([1,0,0,0,1,0,1]))
